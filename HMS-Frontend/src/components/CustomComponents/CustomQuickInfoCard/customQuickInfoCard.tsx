d31450974c3f1d9c4b8ef2a30a2f5b3ad12fc21d
Sahil020701 <sahilgobade777@gmail.com>
2025-09-29 00:53:08 +0530

    added folder structure

---
+++ HMS-Frontend/src/components/CustomComponents/CustomQuickInfoCard/customQuickInfoCard.tsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+}
+ from "../../ui/card"
+
+
+function CustomQuickInfoCard() {
+  return (
+    <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+  )
+
+}
+
+export default CustomQuickInfoCard
