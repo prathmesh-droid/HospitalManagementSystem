cd72c62df26e5297c26c16d474914535dfd47962
Sahil020701 <sahilgobade777@gmail.com>
2025-09-29 22:58:48 +0530

    sidebar updates

--- HMS-Frontend/src/pages/dashboard.tsx
+++ HMS-Frontend/src/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import CustomQuickInfoCard from "../components/CustomComponents/CustomQuickInfoC
 import QuickActionSection from "../sections/quickActions/quickActionSection";
 import { CarouselSpacing } from "../sections/quickInfo/quickInfoSection";
 import RecentAppointmentsSection from "../sections/recentAppointments/recentAppointmentsSection";
-
+import { SidebarTrigger } from "../components/ui/sidebar";
 export default function Dashboard() {
     return (
         <div>
