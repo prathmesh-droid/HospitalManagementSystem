0cc92f87e6807211a40b770abdc617d56fabe439
Sahil020701 <sahilgobade777@gmail.com>
2025-09-29 22:03:40 +0530

    Dashboard added

---
+++ HMS-Frontend/src/pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import CustomQuickInfoCard from "../components/CustomComponents/CustomQuickInfoCard/customQuickInfoCard";
+import QuickActionSection from "../sections/quickActions/quickActionSection";
+import { CarouselSpacing } from "../sections/quickInfo/quickInfoSection";
+import RecentAppointmentsSection from "../sections/recentAppointments/recentAppointmentsSection";
+
+export default function Dashboard() {
+    return (
+        <div>
+            <CarouselSpacing/>
+            <div className="flex gap-2">
+            <RecentAppointmentsSection></RecentAppointmentsSection>
+            <QuickActionSection></QuickActionSection>
+            </div>
+        </div>
+    )
+}
